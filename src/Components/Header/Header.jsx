e2e36af7afaf43d8acd5a79a944f160677f8cefb
tushar-454 <imtushar454@gmail.com>
2023-09-14 21:51:53 +0600

    header part completed

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <div className='title text-center py-8'>
+        <h1 className='text-black text-4xl font-bold'>Course Registration</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ export default {
         blue: '#2F80ED',
       },
     },
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
   },
   plugins: [],
 };
