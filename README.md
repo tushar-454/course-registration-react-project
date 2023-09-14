ec4ea3bb9e9ca35b969e339a45ffe698bab0c88b
tushar-454 <imtushar454@gmail.com>
2023-09-14 21:29:02 +0600

    almost project setup ok and check git push

---
+++ README.md
@@ -0,0 +1,5 @@
+# <u>This is a assignment project ...</u>
+
+## **Name:** _Course Registration._
+
+#### Here I am giving all question answer.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        body: 'F3F3F3',
+        black: '#1C1B1B',
+        gray: '#1C1B1B99',
+        darkGray: '#1C1B1BCC',
+        blue: '#2F80ED',
+      },
+    },
+  },
+  plugins: [],
+};
